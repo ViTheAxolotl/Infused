2fa6e623344a14cae0da2e17c9f12cdc80f420ef
Vi <Vitheaxolotl@gmail.com>
2025-01-25 16:52:21 -0500

    logic

--- js/mapControler.js
+++ js/mapControler.js
@@ -1320,7 +1320,7 @@ function handleUseAction(targets)
             
             if(display){display += `\nAccurcy: ${accurcy} to Hit.\nOn Hit: ${damage} Damage.\n`;}
             else{display = `${toTitleCase(wholeChar[player]["currentToken"])} cast,\n${lastUse} on `;
-            if(targets == "self"){display += "themself, ";} else{for(key in targets){display += `${targets[key].title.slice(0, title.indexOf(":"))}, `}}
+            if(targets == "self"){display += "themself, ";} else{for(key in targets){display += `${targets[key].title.slice(0, targets[key].title.indexOf(":"))}, `}}"".
             display = display.slice(0, display.length - 2);
             display += `\n${useInfo}\nAccurcy: ${accurcy} to Hit.\nOn Hit: ${damage} Damage.\n`;}
     
