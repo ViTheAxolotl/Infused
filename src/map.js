93c475a993229505c58976bfea888b6ac145b763
Vi <Vitheaxolotl@gmail.com>
2025-02-01 15:18:05 -0500

    possition?

--- src/map.js
+++ src/map.js
@@ -140,22 +140,11 @@ function init()
     setInterval(timer, 100);
 
     document.getElementById("helpBtn").onclick = handleCharClick;
-    
-    if(rect.width < 999)
-    {
-        mapSize = rect.width;
-        bumper = 9; 
-        distance = Math.round(mapSize / 14);
-        movement = distance - 4; 
-    }
 
-    else
-    {
-        mapSize = (rect.width * (8 / 10));
-        bumper = Math.round(rect.width / 10) + 2;
-        distance = Math.round(mapSize / 14);
-        movement = distance - 6;
-    } 
+    mapSize = (rect.width * (8 / 10));
+    bumper = Math.round(rect.width / 10) + 2;
+    distance = Math.round(mapSize / 27);
+    movement = distance - 6;
 
     let disAndBum = distance + bumper;
     pos = [disAndBum, disAndBum + movement, disAndBum + (movement * 2), disAndBum + (movement * 3), disAndBum + (movement * 4), disAndBum + (movement * 5), disAndBum + (movement * 6), disAndBum + (movement * 7), disAndBum + (movement * 8), disAndBum + (movement * 9), disAndBum + (movement * 10), disAndBum + (movement * 11), disAndBum + (movement * 12)];
