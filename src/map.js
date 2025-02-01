aa654ca22f68dcdc462cfcc249bbeb4fdd7b29e6
Vi <Vitheaxolotl@gmail.com>
2025-02-01 14:55:34 -0500

    ?

--- src/map.js
+++ src/map.js
@@ -78,22 +78,25 @@ let mouseDown = false;
 let startX, scrollLeft;
 let slider = gridMap;
 
-const startDragging = (e) => {
-  mouseDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+const startDragging = (e) => 
+{
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
 }
 
-const stopDragging = (e) => {
-  mouseDown = false;
+const stopDragging = (e) => 
+{
+    mouseDown = false;
 }
 
-const move = (e) => {
-  e.preventDefault();
-  if(!mouseDown) { return; }
-  const x = e.pageX - slider.offsetLeft;
-  const scroll = x - startX;
-  slider.scrollLeft = scrollLeft - scroll;
+const move = (e) => 
+{
+    e.preventDefault();
+    if(!mouseDown) { return; }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
 }
 
 // Add the event listeners
