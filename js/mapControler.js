9c72a3bf2ea29c180d8033c674004d44be870400
Vi Snyder <77396839+ViTheAxolotl@users.noreply.github.com>
2025-02-04 17:03:51 -0500

    Update mapControler.js

--- js/mapControler.js
+++ js/mapControler.js
@@ -564,7 +564,7 @@ function changeValue()
                 if (zoomLevel < 0.1){zoomLevel = 0.1;}
             }
 
-            document.getElementById("gridMap").style.transition = `transform ${zoomLevel}s`;
+            document.getElementById("gridMap").style.transform = `scale(${zoomLevel})`;
             break;
         
         case "title":
