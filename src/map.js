19087c27cde91ec281319ea9993f33da8de4cd44
Vi <Vitheaxolotl@gmail.com>
2025-02-06 15:39:28 -0500

    1.75

--- src/map.js
+++ src/map.js
@@ -152,7 +152,7 @@ function init()
     else
     {
         mapSize = (trueMapSize * (8 / 10));
-        bumper = Math.round(trueMapSize / 27) * 1.8;
+        bumper = Math.round(trueMapSize / 27) * 1.75;
         distance = Math.round(mapSize / 27);
         movement = distance - 4;
     } 
