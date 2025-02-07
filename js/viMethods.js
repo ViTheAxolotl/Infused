869ad5347e109f61e01eeb1ddd0f98146f4bb4ab
Vi <Vitheaxolotl@gmail.com>
2025-02-06 19:20:36 -0500

    why

--- js/viMethods.js
+++ js/viMethods.js
@@ -40,7 +40,7 @@ export function setMapValue()
 
     else if(rect.width < 1400)
     {
-        movement = distance * 1.177;
+        movement = distance * 1.1776;
     }
 
     let disAndBum = distance + bumper;
