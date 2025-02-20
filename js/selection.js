809f189eebbcaa265894d7bc79005a32c91e22b1
Vi <Vitheaxolotl@gmail.com>
2025-02-20 15:26:30 -0500

    ...

--- js/selection.js
+++ js/selection.js
@@ -91,7 +91,7 @@ function setUpCharacters()
         elem.classList = "hide";
     }
 
-    if(imgs["tokens"][`${player}-`]){people.push(`${player.toLowerCase()}-`);}
+    if(imgs["tokens"][`${player}-`]){people.push(`${player}-`);}
 
     div.appendChild(char);
 
