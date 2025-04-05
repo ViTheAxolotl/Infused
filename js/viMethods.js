f04064e4ad60365549ea4685b1d90051149e8d63
Vi <Vitheaxolotl@gmail.com>
2025-04-05 18:57:15 -0400

    ?

--- js/viMethods.js
+++ js/viMethods.js
@@ -52,7 +52,7 @@ export function setMapValue()
     else if(screen.width < 1200)
     {
         bumper = Math.round(trueMapSize / 26) * 0.35;
-        movement = distance * 1.28;
+        movement = distance * 1.27;
         bubble = -54;
     }
 
