ee76490c61c95d17c1ff4baf05deb13a8af1dd87
Vi <Vitheaxolotl@gmail.com>
2025-05-02 17:18:46 -0400

    math

--- js/mapControler.js
+++ js/mapControler.js
@@ -256,7 +256,7 @@ function sendMessageToDisplay(message)
 
 function handleGridClick(e)
 {
-    let bubbleDB = {id : `${player}-bubble`, x : (e.offsetX - map.bubble) * (zoomLevel/100), y : (e.offsetY - map.bubble) * (zoomLevel/100), size : 1, src : imgs["borders"][wholeDb[wholeChar[player]["currentToken"]].border]};
+    let bubbleDB = {id : `${player}-bubble`, x : (e.offsetX * (zoomLevel/100) - map.bubble), y : (e.offsetY * (zoomLevel/100) - map.bubble), size : 1, src : imgs["borders"][wholeDb[wholeChar[player]["currentToken"]].border]};
     if(bubbleDB.src.includes("invisible")){bubbleDB.src = imgs["borders"]["blue"];}
     setDoc(`bubbles/${bubbleDB.id}`, bubbleDB);
 }
