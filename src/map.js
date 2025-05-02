bb02704a104247e706e2b392baf4255d4281b75f
Vi <Vitheaxolotl@gmail.com>
2025-05-02 18:34:44 -0400

    ?

--- src/map.js
+++ src/map.js
@@ -145,7 +145,7 @@ const move = (e) =>
 
 let startX2, scrollLeft2;
 let startY2, scrollUp2;
-let slider2 = document.getElementById("sheet");
+let slider2 = document.getElementById("statSheet");
 
 const startDragging2 = (e) => 
 {
