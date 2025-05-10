27483f1a8dc7d3da4c27318b0585b895b711e80e
Vi <Vitheaxolotl@gmail.com>
2025-05-10 14:44:00 -0400

    ?

--- js/stats.js
+++ js/stats.js
@@ -46,6 +46,7 @@ function init()
                 if(stat.id == "spellBonus"){let bonus = statFormat(parseInt(wholeChar[player]["stats"][wholeChar[player]["stats"]["spellAbility"]]) + parseInt(wholeChar[player]["stats"]["proficiency"])); stat.innerHTML = bonus; setDoc(`playerChar/${player}/stats/spellBonus`, bonus);}
                 else if(stat.id == "spellDC"){let dc = statFormat(parseInt(wholeChar[player]["stats"][wholeChar[player]["stats"]["spellAbility"]]) + parseInt(wholeChar[player]["stats"]["proficiency"]) + 8); stat.innerHTML = dc; setDoc(`playerChar/${player}/stats/spellDC`, dc);}
                 else if(stat.id == "proficiency"){let prof = statFormat(Math.ceil(parseInt(wholeChar[player]["stats"]["lv"])/4)+1); setDoc(`playerChar/${player}/stats/proficiency`, prof); stat.innerHTML = prof;}
+                else if(stat.id == "name"){stat.innerHTML = player;}
                 else if(stat.id.includes("Save")){continue;}
                 else if(["spellAbility", "lv"].includes(stat.id)){stat.value = wholeChar[player]["stats"][stat.id];}
                 else if(stat.value == ""){stat.value = wholeChar[player]["stats"][stat.id]; if(!["profAndLang", "infusion", "feats", "equipment"].includes(stat.id)){stat.style.minWidth = stat.value.length + 2 + "ch";}}
