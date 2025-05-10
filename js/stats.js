9992f9b922c65715ccbe57f37f292310c3eae3b4
Vi <Vitheaxolotl@gmail.com>
2025-05-10 14:24:26 -0400

    ?

--- js/stats.js
+++ js/stats.js
@@ -47,7 +47,7 @@ function init()
                 else if(stat.id == "spellDC"){let dc = statFormat(parseInt(wholeChar[player]["stats"][wholeChar[player]["stats"]["spellAbility"]]) + parseInt(wholeChar[player]["stats"]["proficiency"]) + 8); stat.innerHTML = dc; setDoc(`playerChar/${player}/stats/spellDC`, dc);}
                 else if(stat.id == "proficiency"){let prof = statFormat(Math.ceil(parseInt(wholeChar[player]["stats"]["lv"])/4)+1); setDoc(`playerChar/${player}/stats/proficiency`, prof); stat.innerHTML = prof;}
                 else if(stat.id.includes("Save")){continue;}
-                else if(stat.id == "spellAbility"){stat.value = wholeChar[player]["stats"][stat.id];}
+                else if(["spellAbility", "lv"].includes(stat.id)){stat.value = wholeChar[player]["stats"][stat.id];}
                 else if(stat.value == ""){stat.value = wholeChar[player]["stats"][stat.id]; if(!["profAndLang", "infusion", "feats", "equipment"].includes(stat.id)){stat.style.minWidth = stat.value.length + 2 + "ch";}}
                 else{stat.innerHTML = wholeChar[player]["stats"][stat.id];}
             }
