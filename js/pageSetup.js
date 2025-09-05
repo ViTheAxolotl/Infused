14a812f81ccab14c6c5dc30f355bed06785a3fc6
Vi <Vitheaxolotl@gmail.com>
2025-09-05 16:59:47 -0400

    ?

--- js/pageSetup.js
+++ js/pageSetup.js
@@ -84,7 +84,6 @@ function navBarSetup()
         <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
             <ul class="navbar-nav me-auto my-2 my-lg-0 " style="--bs-scroll-height: 100px;"> 
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}index.html">Infused</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}questAndNotes.html">Quests & Personal Notes</a></li> 
                 <li class="nav-item dropdown"> 
                     <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Dashboard
@@ -94,6 +93,7 @@ function navBarSetup()
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}map.html">Dashboard</a></li>
                     </ul>
                 </li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}questAndNotes.html">Quests & Personal Notes</a></li> 
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}skillTree.html">Skill Tree</a></li> 
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}recap.html">Sessions Recap</a></li> 
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}itemIndex.html">Magic Item Index</a></li>);    
