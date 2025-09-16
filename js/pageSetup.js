0308fcc46d804bb152b303a0dba510bf280ad7f7
Vi <Vitheaxolotl@gmail.com>
2025-09-16 16:58:04 -0400

    no quest

--- js/pageSetup.js
+++ js/pageSetup.js
@@ -93,7 +93,6 @@ function navBarSetup()
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}map.html">Dashboard</a></li>
                     </ul>
                 </li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}questAndNotes.html">Quests & Personal Notes</a></li> 
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}recap.html">Sessions Recap</a></li> 
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="${mainLocation}itemIndex.html">Magic Item Index</a></li>);    
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="https://vitheaxolotl.github.io/sheets/index.html">Standalone Character Sheets</a></li>);    
