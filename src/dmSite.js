22a8c8f85f6709fc8fe4b8ed666e9ca48ba69239
Vi <Vitheaxolotl@gmail.com>
2026-05-19 16:11:17 -0400

    ?

--- src/dmSite.js
+++ src/dmSite.js
@@ -1,6 +1,7 @@
 import { ref, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 import { toTitleCase, auth, database, setDoc, deleteDoc, returnHpImage, placeBefore, createLabel, wait, storage } from '../js/viMethods.js';
 import { getStorage, uploadBytes, getDownloadURL, ref as sRef } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
+import { imageCompression } from "https://cdn.jsdelivr.net/npm/browser-image-compression@2.0.2/dist/browser-image-compression.js";
 
 let fiveButtons = [];
 let db;
